86391f04c8e8dd9ba400aee635c2aae176091cf5
DAIMUVG <71652308+DAIMUVG@users.noreply.github.com>
2021-02-13 23:42:29 -0600

    Add files via upload

--- Main_lab_2.s
+++ Main_lab_2.s
@@ -7,14 +7,14 @@
 ;Hardware:	LEDs en el puerto A y C, Botones en el Puerto B
 ;
 ;Creado:	8 feb, 2021
-;Ultima modificacion:  feb, 2021
+;Ultima modificacion: 13 feb, 2021
 
     
 PROCESSOR 16F887
 #include <xc.inc>
 
 ; Configuration word 1
-  CONFIG  FOSC = XT	        ; Oscilador interno sin salida 
+  CONFIG  FOSC = XT	         
   CONFIG  WDTE = OFF            ; WDT disabled (reinicio repetitivo del PIC)
   CONFIG  PWRTE = ON            ; PWRT enabled (espera de 72ms al iniciar)
   CONFIG  MCLRE = OFF           ; El pin de MCLR se utiliza como I/O
@@ -32,8 +32,6 @@ PROCESSOR 16F887
 
   PSECT udata_bank0 ;common memory
   cont_small: DS 1 ;1 byte
-  PA:	      DS 1 ;1 byte
-  PC:	      DS 1 ;1 byte
     
   PSECT resVect, class=CODE, abs, delta=2
 
@@ -69,49 +67,50 @@ main:					;Configuración de los puertos
     movlw	11111111B		;Dejo todos como los puertos como botones aunque use solo los primeros
     movwf	TRISB
     
-    banksel	PORTA
-    clrf	PORTA
+    banksel	PORTA			;Llamo al banco de memoria donde estan los PORT
+    clrf	PORTA			;Limpio los puertos
     clrf	PORTC
     clrf	PORTD
     clrf	PORTB
     
 ;---------------Loop Principal-------------------------
 Loop:
-    call	delay_small
-    btfsc	PORTB, 0
-    call	inc_porta
+    call	delay_small		;El delay solo para que espere un tiempo
+    btfsc	PORTB, 0		;Reviso el pin RB0
+    call	inc_porta		;llamo al modulo para incrementar el puerto A
     
-    btfsc	PORTB, 1
-    call	dec_porta
+    btfsc	PORTB, 1		;Reviso el pin RB1
+    call	dec_porta		;llamo al modulo para decrementar el puerto A
     
-    btfsc	PORTB, 2
-    call	inc_portc
+    btfsc	PORTB, 2		;Reviso el pin RB2
+    call	inc_portc		;llamo al modulo para incrementar el puerto C
     
-    btfsc	PORTB, 3
-    call	dec_portc
+    btfsc	PORTB, 3		;Reviso el pin RB3
+    call	dec_portc		;llamo al modulo para decrementar el puerto C
     
-    btfsc	PORTB, 4
-    call	Suma
+    btfsc	PORTB, 4		;Reviso el pin RB4
+    call	Suma			;llamo al modulo donde se encuentra los operadores de la suma
     
-    btfsc	STATUS, 1
-    bsf		PORTD, 5
+    btfsc	STATUS, 1		;Reviso si existe un overflow
+    bsf		PORTD, 5		;Enciendo el pin si hay un overflow
     
-    btfss	STATUS, 5
-    bcf		PORTD, 5
+    btfss	STATUS, 5		;Reviso si existe un overfow
+    bcf		PORTD, 5		;Apago el pin si no hay	un overflow
     
-    goto	Loop
+    goto	Loop			;Regreso al loop
     
+;-------------------------Modulos-----------------------------------------------------------------------    
 inc_porta:
-    btfsc	PORTB, 0
-    goto	$-1
-    incf	PORTA, F
-    return
+    btfsc	PORTB, 0		;Antirebote del boton
+    goto	$-1			
+    incf	PORTA, F		;Incremento el puerto
+    return				;regresa al loop principal
 
 dec_porta:
-    btfsc	PORTB, 1
+    btfsc	PORTB, 1		;Antirebote del boton
     goto	$-1
-    decf	PORTA, F
-    return
+    decf	PORTA, F		;decremento el puerto
+    return				;regresa al loop principal
 	
 inc_portc:
     btfsc	PORTB, 2
@@ -127,12 +126,12 @@ dec_portc:
 ;---------------------SUMA-----------------------------------
     
 Suma:
-    btfsc	PORTB, 4
+    btfsc	PORTB, 4		;Antirebote del boton
     goto	$-1
-    movf	PORTA, w
-    addwf	PORTC, w
-    movwf	PORTD
-    return
+    movf	PORTA, w		;Mueve el valor en el puerto A a w
+    addwf	PORTC, w		;Suma el valor de w y el del puerto C y el nuevo valor lo mete de nuevo a W
+    movwf	PORTD			;Mueve el valor de w al puerto D
+    return				;Regresa al loop principal
 
 delay_small:
     movlw	249		;valor inicial del contador
